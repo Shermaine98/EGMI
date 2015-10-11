ea72f13f9bda697b426979fd57a7372d846402d9
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-10-11 13:55:28 +0800

    nknknkn

--- web/js/searchSupplier.js
+++ web/js/searchSupplier.js
@@ -21,6 +21,10 @@ function autoCompleteSupplierItem() {
     var supplierName = document.getElementById('supplierName').value;
     if (supplierName == "") {
         window.alert("Please Input Supplier")
+        $('input:text').focus(
+    function(){
+        $('#itemNameSupplier').val('');
+    });
     } else {
         $("#itemNameSupplier").devbridgeAutocomplete({
             serviceUrl: 'SearchItemSuppliersServlet',
@@ -66,10 +70,9 @@ function getSupplierItem() {
             success: function (data) {
 
                 if (i == 1) {
-                    $('#data').append('<tr> <th ></th>  <th style="text-align: center; vertical-align: middle">Item Name</th> <th style="text-align: center; vertical-align: middle" class="input">Unit Price</th><th style="text-align: center">Quantity</th><th style="text-align: center">Total Quantity Price</th></tr><th style="text-align: center">Note/th></tr>');
-                }
+                    $('#data').append('<tr><th></th><th>Item Name</th> <th> Unit Price</th> <th>Quantity</th> <th>Total Quantity Price </th> <th>Note</th></tr>'); }
 
-                $('#data').append('<tr><td> <input type="checkbox" name="chk" /> </td><td><input type="hidden" name = "itemCode"/><input type="text" id= itemName[] value="' + data[0].itemName + '"/> </td><td><input type="text" name = "unitPrice" value="' + data[0].unitPrice + '"/></td><td><input type="number" name = "volumeQty" onkeypress="return event.charCode >= 48 && event.charCode <= 57"/><td><input type="text" name = "Total Quantity Price"  /></td><td><input type="hidden" name = "receivingStatus" value="pending" /></td><td><input type="hidden" name = "reconcileStatus" value="pending" /></td><td><input type="text" name = "note"/></td>'  + '</tr>');
+                $('#data').append('<tr><td> <input type="checkbox" name="chk" /> </td><td><input type="hidden" name = "itemCode"/><input type="text" id= itemName[] value="' + data[0].itemName + '"/> </td><td><input type="text" name = "unitPrice" value="' + data[0].unitPrice + '"/></td><td><input type="number" name = "volumeQty" onkeypress="return event.charCode >= 48 && event.charCode <= 57"/><td><input type="text" name = "Total Quantity Price"  /></td><td><input type="hidden" name = "receivingStatus" value="pending" /><input type="hidden" name = "reconcileStatus" value="pending" /><input type="text" name = "note"/></td>'  + '</tr>');
                 i++;
             }, error: function (XMLHttpRequest, textStatus, exception) {
                 alert(XMLHttpRequest.responseText);
