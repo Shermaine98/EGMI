4071162f5e53d3263655c06c99e5ef89263a73ef
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-10-27 21:40:37 +0800

    Edited path for printing

--- src/java/Controller_Reports/PrintConsumption.java
+++ src/java/Controller_Reports/PrintConsumption.java
@@ -6,17 +6,10 @@
 package Controller_Reports;
 
 import Controller.BaseServlet;
-import Controller.PurchaseOrderSerlvet;
-import DAO.ConsumptionReportDAO;
-import DAO.PurchaseOrderDAO;
 import Database.DBConnectionFactory;
-import Model.PurchaseOrder;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -24,20 +17,16 @@ import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import net.sf.jasperreports.engine.JRException;
-
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.view.JasperViewer;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 
 
 /**
@@ -45,22 +34,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
  * @author Geraldine
  */
 public class PrintConsumption extends BaseServlet {
-
+  
     @Override
     public void servletAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-         String relativeWebPath = "/Reports";
-         String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
-         
-         request.setAttribute(absoluteDiskPath, absoluteDiskPath);
-         
-        ConsumptionReportDAO dao = new ConsumptionReportDAO();
+   
          String productionNumber = request.getParameter("printPONumber");
         try {
             showReport(Integer.parseInt(productionNumber));
         } catch (JRException ex) {
             Logger.getLogger(PrintConsumption.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         ServletContext context = getServletContext();
         RequestDispatcher rd = context.getRequestDispatcher("/ViewConsumptionReportServlet");
         rd.forward(request, response);
@@ -68,24 +52,35 @@ public class PrintConsumption extends BaseServlet {
 
  public void showReport(int productionNumber) throws JRException {           
         try {
-           String relativeWebPath = "/Reports";
-           String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
-           File f = new File(absoluteDiskPath, "ConsumptionReport.jrxml");
-            System.out.println(f);
-            JasperReport jr = JasperCompileManager.compileReport(f.getAbsolutePath()); 
             
             DBConnectionFactory myFactory = DBConnectionFactory.getInstance();
             Connection conn = myFactory.getConnection();
+            
+          //watever this is
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
 
+          //get absolute path  
+            String relativeWebPath = "/Reports";
+            String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
+            File f = new File(absoluteDiskPath, "ConsumptionReport.jrxml");
+          
+            JasperReport jr = JasperCompileManager.compileReport(f.getAbsolutePath()); 
+  
             Map map = new HashMap();
             map.put("prodNum", productionNumber);
-
-           // JasperReport jr = JasperCompileManager.compileReport(reportPath+"/ConsumptionReport.jrxml");
-            //Fill the report with parameter, connection and the stream reader     
-            JasperPrint jp = JasperFillManager.fillReport(jr, map, conn);
-
-            JasperExportManager.exportReportToPdfFile(jp, "/Users/shermainesy/NetBeansProjects/EGMI/web/Reports/ConsumptionReport.pdf");
-            JasperViewer.viewReport(jp, true);
+            //Fill the report with parameter, connection and the stream reader   
+  
+           // printing
+           JasperPrint jp = JasperFillManager.fillReport(jr, map, conn);
+             
+        
+          httpServletResponse.addHeader("Content-disposition", "attachment; filename=ConsumptionReport.pdf");
+          ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+          JasperExportManager.exportReportToPdfStream(jp,servletOutputStream);
+          FacesContext.getCurrentInstance().responseComplete();
+            
+          //  JasperExportManager.exportReportToPdfFile(jp, "ConsumptionReport.pdf");
+            //            JasperViewer.viewReport(jp, true);
         } catch (Exception e) {
             e.printStackTrace();
         }
