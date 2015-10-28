3e8fc5691f819b122aa74328a71cfff75e2d9cab
shermainesy <shermaine_sy@dlsu.edu.ph>
2015-10-28 23:35:38 +0800

    working printing

--- src/java/Controller_Reports/PrintConsumption.java
+++ src/java/Controller_Reports/PrintConsumption.java
@@ -7,7 +7,6 @@ package Controller_Reports;
 
 import Controller.BaseServlet;
 import Database.DBConnectionFactory;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,19 +17,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletOutputStream;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JasperRunManager;
 
 /**
  *
@@ -40,57 +33,36 @@ public class PrintConsumption extends BaseServlet {
 
     @Override
     public void servletAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DBConnectionFactory myFactory = DBConnectionFactory.getInstance();
+        Connection conn = myFactory.getConnection();
 
+        JasperReport jasperReport = null;
+        //  JasperDesign jasperDesign = null;
+        String productionNumber = request.getParameter("printPONumber");
+        int productionNumberInt = Integer.parseInt(productionNumber);
+
+        Map map = new HashMap();
+        map.put("prodNum", productionNumberInt);
+
+        String path = getServletContext().getRealPath("Reports/ProcurementReports/");
+        // jasperDesign = JRXmlLoader.load(path + "ConsumptionReport.jrxml");
+        InputStream f = new FileInputStream(new File(path + "/ConsumptionReport.jrxml"));
         try {
-            String productionNumber = request.getParameter("printPONumber");
-            showReport(Integer.parseInt(productionNumber), response);
-
-            String pdfFileName = "C:\\Users\\Geraldine\\Desktop\\EGMI\\web\\Reports\\ProcurementReports\\ConsumptionReport.pdf";
-            File pdfFile = new File(pdfFileName);
-
-            response.setContentType("application/pdf");
-            response.setHeader("Content-Disposition", "attachment; filename=ConsumptionReport.pdf");
-            response.setContentLength((int) pdfFile.length());
-
-            FileInputStream fileInputStream = new FileInputStream(pdfFile);
-            OutputStream responseOutputStream = response.getOutputStream();
-            int bytes;
-            while ((bytes = fileInputStream.read()) != -1) {
-                responseOutputStream.write(bytes);
-            }
-            
-//            ServletContext context = getServletContext();
-//            RequestDispatcher rd = context.getRequestDispatcher("/Reports/ProcurementReports/ConsumptionReport.jsp");
-//            rd.forward(request, response);
+            jasperReport = JasperCompileManager.compileReport(f);
         } catch (JRException ex) {
             Logger.getLogger(PrintConsumption.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
 
-    public void showReport(int productionNumber, HttpServletResponse response) throws JRException {
+        byte[] byteStream = null;
         try {
-
-            DBConnectionFactory myFactory = DBConnectionFactory.getInstance();
-            Connection conn = myFactory.getConnection();
-            response.setContentType("application/pdf");
-
-            //get absolute path  
-            String relativeWebPath = "/ProcurementReports/Reports";
-            String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
-            File f = new File(absoluteDiskPath, "ConsumptionReport.jrxml");
-
-            JasperReport jr = JasperCompileManager.compileReport(f.getAbsolutePath());
-
-            //Fill the report with parameter, connection and the stream reader   
-            Map map = new HashMap();
-            map.put("prodNum", productionNumber);
-
-            // printing
-            JasperPrint jp = JasperFillManager.fillReport(jr, map, conn);
-            JasperExportManager.exportReportToPdfFile(jp, "/Users/shermainesy/NetBeansProjects/EGMI/web/Reports/ConsumptionReport.pdf");
-            FacesContext.getCurrentInstance().responseComplete();
-        } catch (Exception e) {
-            e.printStackTrace();
+            byteStream = JasperRunManager.runReportToPdf(jasperReport, map, conn);
+        } catch (JRException ex) {
+            Logger.getLogger(PrintConsumption.class.getName()).log(Level.SEVERE, null, ex);
         }
+        OutputStream outStream = response.getOutputStream();
+        response.addHeader("content-disposition", "attachment; filename=this.pdf");   
+        response.setContentType("application/pdf");
+        response.setContentLength(byteStream.length);
+        outStream.write(byteStream, 0, byteStream.length);
     }
 }
--- web/js/searchItem.js
+++ web/js/searchItem.js
@@ -53,7 +53,7 @@ function getItem() {
                                                         <td>\n\
                     <input type="hidden" name = "itemCode" id="itemCode[]" value="' + data[0].ItemCode + '"/> \n\
                  <input type="text" class="transparentBg" readonly id="itemName[]" name = "itemName" value="' + data[0].itemName + '"/></td> \n\
-                <td><input class="transparentBg" type="number" id="itemConsumption[]" name="itemConsumption" onkeypress="return event.charCode >= 48 && event.charCode <= 57"  /></td> \n\
+                <td><input class="transparentBg" value="0" type="number" min="1" id="itemConsumption[]" name="itemConsumption" onkeypress="return event.charCode >= 48 && event.charCode <= 57"  /></td> \n\
             <td><input class="transparentBg" name="unitMeasurement" id="unitMeasurement[]" value="'+data[0].unitMeasurement +'" /> </td>'
                         + '</tr>');
                 document.getElementById("buttons").style.visibility = 'visible';
